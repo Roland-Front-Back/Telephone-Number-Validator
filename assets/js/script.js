fbf0d25ee745ed800c848f2805be7871e6a4a433
Roland Amoguis <rolandamoguis21@gmail.com>
2025-03-06 19:52:18 +0800

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const userInput = document.getElementById("user-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result-dic")
+
+const numPattern = /(\d{3})[-.\s]?(\d{3})[-.\s]?(\d{4})/g;
