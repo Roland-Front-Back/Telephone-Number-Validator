c03dc304706143f62f89280fe6781ba428406fc5
Roland Amoguis <rolandamoguis21@gmail.com>
2025-03-08 14:52:51 +0800

    Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
-const result = document.getElementById("result-dic")
+const result = document.getElementById("result-div");
 
-const numPattern = /(\d{3})[-.\s]?(\d{3})[-.\s]?(\d{4})/g;
+const numPattern =
+  /^(?:1[ ]?)?(?:\(([0-9]{3})\)[ -]?|([0-9]{3})[ -]?)([0-9]{3})[ -]?([0-9]{4})$/;
+
+const isValid = (num) => {
+  numPattern.lastIndex = 0; 
+  return numPattern.test(num);
+
+}
+checkButton.addEventListener("click", () => {
+  if (userInput.value === "") {
+    alert("Please provide a phone number");
+    return;
+  }
+
+  result.innerHTML = isValid(userInput.value)
+    ? `<span class="text">Valid US number:</span><span class="value">${userInput.value}</span>`
+    : `<span class="text">Invalid US number:</span> <span class="value">${userInput.value}</span>`;
+});
+
