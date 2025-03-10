210c40cce79a784ab88207f4339dbe68e7e245e8
Roland Amoguis <rolandamoguis21@gmail.com>
2025-03-10 22:01:20 +0800

    Added clear button, list, ul, features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,48 @@
 const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
-const result = document.getElementById("result-div");
+const clearButton = document.getElementById("clear-btn");
+const results = document.getElementById("results-div");
+
+let listInput = [];
 
 const numPattern =
   /^(?:1[ ]?)?(?:\(([0-9]{3})\)[ -]?|([0-9]{3})[ -]?)([0-9]{3})[ -]?([0-9]{4})$/;
 
-const isValid = (num) => {
-  numPattern.lastIndex = 0; 
-  return numPattern.test(num);
+const checkInput = () => {
+  const inputList = listInput
+    .map((input) => {
+      const isValidNumber = isValid(input);
+      return `
+    <li>
+    <span class="text">${isValidNumber ? "Valid" : "Invalid"} US number: </span>
+    <span class="value">${input}</span>
+    </li>
+    `;
+    })
+    .reverse()
+    .join("\n");
 
-}
+  results.innerHTML = `<ul>${inputList}</ul>`;
+};
+
+const isValid = (num) => {
+  return numPattern.test(num);
+};
+
 checkButton.addEventListener("click", () => {
   if (userInput.value === "") {
     alert("Please provide a phone number");
     return;
   }
 
-  result.innerHTML = isValid(userInput.value)
-    ? `<span class="text">Valid US number:</span><span class="value">${userInput.value}</span>`
-    : `<span class="text">Invalid US number:</span> <span class="value">${userInput.value}</span>`;
+  listInput.push(userInput.value);
+  userInput.value = "";
+  checkInput();
 });
 
+const reset = () => {
+  results.innerHTML = "";
+  listInput = [];
+};
+
+clearButton.addEventListener("click", reset);
